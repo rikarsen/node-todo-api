6e45d938de1185197cf9fd5d286d15a6c68446b1
Arsen Babajanyan <arsenbabajanyan95@gmail.com>
2018-06-19 17:07:16 +0400

    Add POST /todos route and refactor mongoose

--- playground/server.js
+++ playground/server.js
@@ -10,16 +10,13 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
-  db.collection('Todos').insertOne({
-    text: 'something',
-    completed: false
-  }, (err, res) => {
-    if (err) {
-      return console.log('Unable to insert todo', err);
-    }
+  db.collection('Todos')
+    .deleteOne({
+      text: 'something',
+    })
+    .then((res) => {
+      console.log(res);
+    });
 
-    console.log(JSON.stringify(res.ops, undefined, 2));
+    client.close();
   });
-
-  client.close();
-});
---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(doc => res.send(doc), err => res.status(400).send(err));
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+// let todo = new Todo({
+//   text: 'something    d'
+// });
+//
+// todo
+//   .save()
+//   .then((doc) => {
+//     console.log('Saved todo', doc);
+//   }, (err) => {
+//     console.log('Unable to save todo', err);
+//   });
+//
+// let user = new User({
+//   email: 'arsenbabajanyan95@gmail.com'
+// });
+//
+// user
+//   .save()
+//   .then((doc) => {
+//     console.log('User saved', doc);
+//   }, (err) => {
+//     console.log('Unable to save user', err);
+//   });
